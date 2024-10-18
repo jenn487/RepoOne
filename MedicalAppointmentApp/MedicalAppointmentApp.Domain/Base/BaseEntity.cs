15c92d114b22b021d79c85630073c5f4e52baef3
jenn487 <jenniferdocuments07@gmail.com>
2024-10-18 00:25:28 -0300

    Actuallizacion

---
+++ MedicalAppointmentApp/MedicalAppointmentApp.Domain/Base/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿
+namespace MedicalAppointmentApp.Domain.Base
+{
+    public abstract class BaseEntity
+    {
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public bool IsActive { get; set; }
+
+    }
+}
---
+++ MedicalAppointmentApp/MedicalAppointmentApp.Domain/Repositories/IBaseRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq.Expressions;
+
+namespace MedicalAppointmentApp.Domain.Repositories
+{
+    // "Task" puede realizar operaciones de manera asincrona
+
+    public interface IBaseRepository<TEntity> where TEntity : class
+        {
+            Task Save(TEntity entity);
+            Task Update(TEntity entity);
+            Task Remove(TEntity entity);
+            Task<TEntity> GetAll();
+            Task GetEntityBy(Type Id);
+            Task<bool> Exists(Expression<Func<TEntity, bool>> filter);
+
+            // Estas son las propiedades comunes para mayoria de entidades
+            DateTime CreatedAt { get; set; }
+            DateTime? UpdatedAt { get; set; }
+            bool IsActive { get; set; }
+        }
+}
